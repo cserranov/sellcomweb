92d56824a36c60daa579a66891d00716fa4efa6b
Carlos Enrique Serrano Varela <141391012+cserranov@users.noreply.github.com>
2024-01-14 19:44:20 -0600

    Add files via upload

--- globe.js
+++ globe.js
@@ -98,9 +98,9 @@ DAT.Globe = function (container, opts) {
         shader = Shaders['earth'];
         uniforms = THREE.UniformsUtils.clone(shader.uniforms);
 
-        uniforms['texture'].value = THREE.ImageUtils.loadTexture(imgDir + 'sellcomweb.gthub.io/world_11zon.webp');
+        uniforms['texture'].value = THREE.ImageUtils.loadTexture(imgDir + 'sellcom_page-main/world_11zon.webp');
        /* uniforms['texture'].value = THREE.ImageUtils.loadTexture(imgDir + 'assets/world.png');*/
-
+       /*uniforms['texture'].value = THREE.ImageUtils.loadTexture(imgDir + 'sellcomweb.gthub.io/world_11zon.webp');*/
         material = new THREE.ShaderMaterial({
 
             uniforms: uniforms,
