7788b8986ccb62c09d4643a36efdd4387ac2f536
Carlos Enrique Serrano Varela <carlos.serrano@sellcom-solutions.com.mx>
2024-01-14 18:39:11 -0600

    sell final

--- globe.js
+++ globe.js
@@ -98,7 +98,7 @@ DAT.Globe = function (container, opts) {
         shader = Shaders['earth'];
         uniforms = THREE.UniformsUtils.clone(shader.uniforms);
 
-        uniforms['texture'].value = THREE.ImageUtils.loadTexture(imgDir + 'sell-com_page-main/world_11zon.webp');
+        uniforms['texture'].value = THREE.ImageUtils.loadTexture(imgDir + 'sellcom_page-main/world_11zon.webp');
        /* uniforms['texture'].value = THREE.ImageUtils.loadTexture(imgDir + 'assets/world.png');*/
        /*uniforms['texture'].value = THREE.ImageUtils.loadTexture(imgDir + 'sellcomweb.gthub.io/world_11zon.webp');*/
         material = new THREE.ShaderMaterial({
